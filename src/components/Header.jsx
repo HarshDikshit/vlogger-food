9d33b673e0976ee66c8cfc8ef5cdb5e25634c7c2
Arit16a <pandey07arpit@gmail.com>
2024-06-14 11:37:31 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <div className=' fixed z-[999] w-full py-1.5 px-10 bg-purple-400 flex justify-between shadow-lg '>
+
+        <div className="logo font-bold text-xl flex
+         items-center">logo</div>
+
+        <div className='items flex gap-5 items-center'>
+        {["About", "Contact Us", "Links","Gallery", "Sign In"].map((item, index)=>(
+           
+            <div className={` cursor-pointer text-md ${index===4 && 'ml-20 bg-white px-4 py-2 flex items-center rounded-full hover:bg-purple-300 hover:text-white '}`}>{item}</div>
+        ))}
+        </div>
+
+
+    </div>
+
+    </>
+   
+  )
+}
+
+export default Header
