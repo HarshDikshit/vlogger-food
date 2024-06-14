9f02d2866f1457bde55abcad8019b6d11da84fdf
Arit16a <pandey07arpit@gmail.com>
2024-06-14 15:02:43 +0530

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Landing from './components/Landing'
+import Body from './components/Body'
 
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
    <Header/>
+   <Landing/>
+   <Body/>
 
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { FaTree } from "react-icons/fa6";
 
 function Header() {
   return (
     <>
     <div className=' fixed z-[999] w-full py-1.5 px-10 bg-purple-400 flex justify-between shadow-lg '>
 
-        <div className="logo font-bold text-xl flex
-         items-center">logo</div>
+        <div className="logo font-bold text-xl flex 
+         items-center gap-5"><FaTree className=' text-red-500'/> logo</div>
 
         <div className='items flex gap-5 items-center'>
         {["About", "Contact Us", "Links","Gallery", "Sign In"].map((item, index)=>(
